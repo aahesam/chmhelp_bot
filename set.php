1f04594bbeeeff728996332e2cf019dd59558cf5
aahesam <45318539+aahesam@users.noreply.github.com>
2018-12-20 17:17:54 +0330

    Update set.php

--- set.php
+++ set.php
@@ -1,6 +1,6 @@
 <?php
-$API_KEY = '237966204:AAExLADwKL5KCi54dRF1nF7xLHH4dGCuewA';
-$hook_url = 'https://mrfenj.xyz/hoseinpor/bot/hoseinporbot.php';
+$API_KEY = '751222329:AAFtXgIeRwPNZCh1plOec1dv5ULeb0pwXoQ';
+$hook_url = 'https://aaahesam.000webhostapp.com/wateri/wateri/set.php';
 $hok="https://api.telegram.org/bot$API_KEY/setWebhook?url=$hook_url";
 echo file_get_contents($hok);
 
